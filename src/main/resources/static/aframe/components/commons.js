07db6109707405635e7290273bbbc7af54ed1a2a
codep <codep@LAPTOP-TOIFVCIS>
2025-01-09 15:56:37 +0900

    0.1.9-v4

---
+++ src/main/resources/static/aframe/components/commons.js
@@ -0,0 +1,18 @@
+
+AFRAME.registerComponent('portal-url', {
+    init: function () {
+        const el = this.el;
+        el.setAttribute('obb-collider', ``);
+        el.addEventListener('obbcollisionstarted', function (e) {
+            console.log('Player collided start ');
+            if (el.hasAttribute('href')) {
+                window.location.href = el.getAttribute('href');
+            }
+        });
+        el.addEventListener('obbcollisionended', function (e) {
+            console.log('Player collided end ');
+        });
+        console.log('collider component initialized : ');
+    }
+});
+
